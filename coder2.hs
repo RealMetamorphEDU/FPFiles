32d93b61fe1024fd59cf52f527327a997aee67d1
RealMetamorph <andr.timchuk@yandex.ru>
2019-04-18 21:18:00 +0300

    correct coder and added coder2

--- coder2.hs
+++ coder2.hs
@@ -5,14 +5,14 @@ import System.Environment
 
 
 genStrHelper sym1 sym2 acc | sym1 == sym2 = acc ++ [sym1]
-                           | sym1 > 'z' = genStrHelper ('!') sym2 (acc ++ [' '])
-                           | True = genStrHelper (succ sym1) sym2 (acc ++ [sym1]) 
+                           | sym1 > '\DEL' = genStrHelper ('\b') sym2 (acc ++ ['\a'])
+                           | True = genStrHelper (succ sym1) sym2 (acc ++ [sym1])
 genStr sym = genStrHelper (succ sym) sym []
 
 genTableHelper acc [] = acc
 genTableHelper acc (x:xs) = genTableHelper (acc ++ [genStr x]) xs
 
-genTable pass = genTableHelper [genStrHelper ' ' 'z' []] pass
+genTable pass = genTableHelper [genStrHelper '\a' '\DEL' []] pass
 
 
 getEncodedSym table num sym = let 
@@ -38,20 +38,23 @@ getDecodedText acc ind len [] table = acc
 getDecodedText acc ind len (x:xs) table | ind > len = getDecodedText (acc ++ [getDecodedSym table 1 x]) (2) len xs table
                                         | True = getDecodedText (acc ++ [getDecodedSym table ind x]) (ind + 1) len xs table
 
+
+encodeTimer text table len time | time <= 0 = text
+                                | True = encodeTimer (getEncodedText [] 1 len text table) table len (time - 1)
 encode pass text = let
                       table = genTable pass
                       len = length pass
                    in
-                      getEncodedText [] 1 len text table
+                      encodeTimer text table len len
 
 
-
-
+decodeTimer text table len time | time <= 0 = text
+                                | True = decodeTimer (getDecodedText [] 1 len text table) table len (time - 1)
 decode pass text = let
                       table = genTable pass
                       len = length pass
                    in
-                      getDecodedText [] 1 len text table
+                      decodeTimer text table len len
 
 
 main = do 
